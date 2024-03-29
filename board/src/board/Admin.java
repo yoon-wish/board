89d32083b1ea3357361361cddc3b7f9f0f76067c
yoon-wish <sowon5685@gmail.com>
2024-03-29 21:27:15 +0900

    [ADD] Admin 클래스 User와 상속관계 갖도록 설정, 변수와 생성자 생성

--- board/src/board/Admin.java
+++ board/src/board/Admin.java
@@ -1,5 +1,16 @@
 package board;
 
-public class Admin {
+public class Admin extends User {
+
+	boolean isAdmin;
+
+	public Admin(String id, String pw) {
+		super(id, pw);
+		isAdmin = true;
+	}
+
+	public boolean getAdmin() {
+		return this.isAdmin;
+	}
 
 }
