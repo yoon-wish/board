2aefaec639ba2d55d6860b66564745236c1bd32a
yoon-wish <sowon5685@gmail.com>
2024-03-29 21:42:00 +0900

    [ADD] PostManager에 User를 받는 ArrayList 선언 및 CRUD 작성

--- board/src/board/PostManager.java
+++ board/src/board/PostManager.java
@@ -1,5 +1,36 @@
 package board;
 
+import java.util.ArrayList;
+
 public class PostManager {
 
+	ArrayList<Post> posts = new ArrayList<>();
+	
+	// CRUD
+	// C
+	public void creatPost(String id, String title, String content, int code) {
+		Post post = new Post(id, title, content, code);
+		posts.add(post);
+	}
+	
+	// R
+	public Post readPost(int index) {
+		return posts.get(index);
+	}
+	
+	// U
+	public void updatePostTitle(int index, String newTitle) {
+		
+	}
+	
+	public void updatePostContent(int index, String newContent) {
+		
+	}
+	
+	// D
+	public void deletePost(int index) {
+		posts.remove(index);
+	}
+	
+	
 }
