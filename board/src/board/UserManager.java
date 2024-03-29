209188d30fcbea061c03c9b635497c06a045b313
yoon-wish <sowon5685@gmail.com>
2024-03-29 21:49:32 +0900

    [ADD] UserManager에 checkDuplId 메소드 작성

--- board/src/board/UserManager.java
+++ board/src/board/UserManager.java
@@ -3,25 +3,34 @@ package board;
 import java.util.ArrayList;
 
 public class UserManager {
-	
+
 	ArrayList<User> group = new ArrayList<>();
-	
+
 	// CRUD
 	// C
 	public void creatUser(String id, String pw) {
 		User user = new User(id, pw);
 		group.add(user);
 	}
-	
+
 	// R
 	public User readUser(int index) {
-		return group.get(index); 
+		return group.get(index);
 	}
-	
+
 	// D
 	public void deleteUser(int index) {
 		group.remove(index);
 	}
-	
-	
+
+	public boolean checkDuplId(String id) {
+		for (int i = 0; i < group.size(); i++) {
+			if (group.get(i).getId().equals(id)) {
+				System.err.println("이미 존재하는 아이디입니다.");
+				return true;
+			}
+		}
+		return false;
+	}
+
 }
