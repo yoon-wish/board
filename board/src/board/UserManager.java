e9f3a004606ad2714f58952fd821dc6d129f4d8f
yoon-wish <sowon5685@gmail.com>
2024-03-29 21:34:57 +0900

    [ADD] User를 받는 ArrayList 선언 및 CRUD 작성

--- board/src/board/UserManager.java
+++ board/src/board/UserManager.java
@@ -1,5 +1,27 @@
 package board;
 
-public class UserManager {
+import java.util.ArrayList;
 
+public class UserManager {
+	
+	ArrayList<User> group = new ArrayList<>();
+	
+	// CRUD
+	// C
+	public void creatUser(String id, String pw) {
+		User user = new User(id, pw);
+		group.add(user);
+	}
+	
+	// R
+	public User readUser(int index) {
+		return group.get(index); 
+	}
+	
+	// D
+	public void deleteUser(int index) {
+		group.remove(index);
+	}
+	
+	
 }
