adb63eaa866f4c5d1ed2731fef540eeac394b219
yoon-wish <sowon5685@gmail.com>
2024-03-29 22:40:34 +0900

    [ADD] Board에 탈퇴처리 작성

--- board/src/board/Board.java
+++ board/src/board/Board.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Board {
 	private Scanner sc = new Scanner(System.in);
-	
+
 	private final int JOIN = 1;
 	private final int LEAVE = 2;
 	private final int LOGIN = 3;
@@ -16,32 +16,32 @@ public class Board {
 	private final int SEARCH = 6;
 	private final int MYPAGE = 7;
 	private final int MANAGER = 8;
-	
+
 	private final int CHECK_LOGIN = 1;
 	private final int CHECK_LOGOUT = 2;
 
 	private final int NOTICE = 1;
 	private final int ALLPOST = 2;
-	
+
 	private int log;
 	private Map<User, ArrayList<Post>> board;
 	private UserManager userManager;
 	private PostManager postManager;
-	
+
 	public Board() {
 		setBoard();
 	}
-	
+
 	private void setBoard() {
 		board = new HashMap<>();
 		userManager = new UserManager();
 		postManager = new PostManager();
 		Admin admin = new Admin("admin", "1234");
 		userManager.createAdmin(admin);
+		board.put(admin, null);
 		log = -1;
 	}
-	
-	
+
 	private int printMenu() {
 		System.out.println("==== Board ====");
 		System.out.println("[1] 회원가입");
@@ -49,13 +49,13 @@ public class Board {
 		System.out.println("[3] 로 그 인");
 		System.out.println("[4] 로그아웃");
 		System.out.println("[5] 작성하기");
-		System.out.println("[6] 조회하기"); 
+		System.out.println("[6] 조회하기");
 		System.out.println("[7] 마이페이지");
 		System.out.println("[8] 관리자");
 
 		return inputNumber("Menu");
 	}
-	
+
 	private int inputNumber(String message) {
 		System.out.print(message + " : ");
 		int number = -1;
@@ -71,8 +71,8 @@ public class Board {
 	private void runMenu(int sel) {
 		if (sel == JOIN && isLogin(CHECK_LOGOUT))
 			join();
-//		else if (sel == LEAVE && isLogin(CHECK_LOGIN))
-//			leave();
+		else if (sel == LEAVE && isLogin(CHECK_LOGIN))
+			leave();
 		else if (sel == LOGIN && isLogin(CHECK_LOGOUT))
 			login();
 		else if (sel == LOGOUT && isLogin(CHECK_LOGIN))
@@ -87,25 +87,41 @@ public class Board {
 //			manager();
 
 	}
-	
+
 	private void join() {
 		String id = inputString("id");
-		if(userManager.checkDuplId(id)) {
+		if (userManager.checkDuplId(id)) {
 			System.err.println("이미 존재하는 아이디입니다.");
 			return;
 		}
-		
+
 		// 유저 추가
 		String pw = inputString("pw");
 		userManager.creatUser(id, pw);
-		
+
 		// 추가한 유저를 board에 put
 		User user = userManager.readUserById(id);
 		board.put(user, null);
-		
+
 		System.out.printf("[%s]님 환영합니다.\n", id);
 	}
-	
+
+	private void leave() {
+		if (log == 0) {
+			System.err.println("관리자 계정은 탈퇴 불가");
+			return;
+		}
+
+		String pw = inputString("pw");
+		
+		User user = userManager.readUser(log);
+		int result = userManager.deleteUser(log, pw);
+		if (result != -1) {
+			board.remove(user);
+			log = -1;
+		}
+	}
+
 	private void login() {
 		String id = inputString("id");
 		String pw = inputString("pw");
@@ -116,34 +132,34 @@ public class Board {
 			return;
 		}
 
-		if(userManager.checkDuplIdAndPw(id, pw)) {
+		if (userManager.checkDuplIdAndPw(id, pw)) {
 			log = index;
 			System.out.println("로그인 성공");
 		}
 	}
-	
+
 	private void logout() {
 		log = -1;
 		System.out.println("로그아웃 완료");
 	}
-	
-	private int printSearchSubMenu(){
+
+	private int printSearchSubMenu() {
 		System.out.println("1) 공지사항");
 		System.out.println("2) 전 체 글");
 		return inputNumber("Menu");
 	}
-	
+
 	private void runSearch(int sel) {
-		if(sel == NOTICE)
+		if (sel == NOTICE)
 			searchNotice();
-		else if(sel == ALLPOST)
+		else if (sel == ALLPOST)
 			searchAllPost();
 	}
-	
+
 	private void temp() {
-		
+
 	}
-	
+
 	private boolean isLogin(int check) {
 		if (check == CHECK_LOGOUT && log != -1) {
 			System.err.println("로그아웃 후 이용가능");
@@ -157,7 +173,7 @@ public class Board {
 
 		return true;
 	}
-	
+
 	private String inputString(String message) {
 		System.out.print(message + " : ");
 		return sc.next();
@@ -171,9 +187,11 @@ public class Board {
 	private String inputStringLine() {
 		return sc.nextLine();
 	}
-	
+
 	public void run() {
 		while (true) {
+			System.out.println(board);
+			System.out.println(userManager.group);
 			runMenu(printMenu());
 		}
 	}
