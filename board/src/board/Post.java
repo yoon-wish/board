9aea409f3019f8d6f3b020f50d9c783f47616ed0
yoon-wish <sowon5685@gmail.com>
2024-03-29 21:30:24 +0900

    [ADD] Post 클래스 변수 선언 및 생성자 작성, getter 생성

--- board/src/board/Post.java
+++ board/src/board/Post.java
@@ -1,5 +1,26 @@
 package board;
 
 public class Post {
-
+	
+	private String title;
+	private String content;
+	private int code;
+	
+	public Post(String title, String content, int code) {
+		this.title = title;
+		this.content = content;
+		this.code = code;
+	}
+	
+	// Admin용 포스트
+	public Post(String title, String content) {
+		this.title = title;
+		this.content = content;
+		this.code = 0000;
+	}
+	
+	public int getCode() {
+		return this.code;
+	}
+	
 }
