5a4fb5f0188bdfe5be06d02f80d17ee55896470d
yoon-wish <sowon5685@gmail.com>
2024-03-29 21:26:14 +0900

    [ADD] user 클래스 변수 생성 및 getter 추가

--- board/src/board/User.java
+++ board/src/board/User.java
@@ -1,5 +1,19 @@
 package board;
 
 public class User {
+	String id;
+	String pw;
 
+	public User(String id, String pw) {
+		this.id = id;
+		this.pw = pw;
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public String getPw() {
+		return this.pw;
+	}
 }
