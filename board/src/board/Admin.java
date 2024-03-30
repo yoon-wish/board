8a42016c4ab03527a6bcf696356d994700b9eeff
yoon-wish <sowon5685@gmail.com>
2024-03-30 14:36:33 +0900

    [Modify] admin에 isAdmin 변수 제거

--- board/src/board/Admin.java
+++ board/src/board/Admin.java
@@ -2,15 +2,9 @@ package board;
 
 public class Admin extends User {
 
-	boolean isAdmin;
 
 	public Admin(String id, String pw) {
 		super(id, pw);
-		isAdmin = true;
-	}
-
-	public boolean getAdmin() {
-		return this.isAdmin;
 	}
 
 }
