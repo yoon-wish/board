b3beb31c0a325baf22359289cab2e274c9ae962b
yoon-wish <sowon5685@gmail.com>
2024-03-30 13:30:13 +0900

    [ADD] Post에 id용 getter 작성

--- board/src/board/Post.java
+++ board/src/board/Post.java
@@ -1,19 +1,19 @@
 package board;
 
 public class Post {
-	
+
 	private String id;
 	private String title;
 	private String content;
 	private String code;
-	
+
 	public Post(String id, String title, String content, String code) {
 		this.id = id;
 		this.title = title;
 		this.content = content;
 		this.code = code;
 	}
-	
+
 	// Admin용 포스트
 	public Post(String title, String content) {
 		this.title = title;
@@ -21,23 +21,30 @@ public class Post {
 		this.code = "0000";
 	}
 	
+	public String getId() {
+		return this.id;
+	}
+
 	public String getCode() {
 		return this.code;
 	}
-	
+
 	public String getTitle() {
 		return this.title;
 	}
+
 	@Override
 	public String toString() {
 		String info = "";
-		info += "--------------------\n";
-		info += "작성자) " + id;
-		info += "\n--------------------";
+		info += "--------------------";
+		if (id != null) {
+			info += "\n작성자) " + id;
+			info += "\n--------------------";
+		}
 		info += "\nTitle : " + title;
 		info += "\nContent : " + content;
 		info += "\n--------------------";
 		return info;
 	}
-	
+
 }
